49a9dc96861b9647c49393f37208cd2c7828eb7e
Denis0827 <wudenis0827@gmail.com>
2025-06-15 20:11:20 -0300

    vecinos

--- src/VecinoMasCercanoSolver.cpp
+++ src/VecinoMasCercanoSolver.cpp
@@ -7,26 +7,29 @@
 
 using namespace std;
 
-// Función auxiliar para obtener un vector de Saving dada un vector de distancias inicial,
-// ordenado de mayor a menor
-vector<Saving> calcularSavings(const vector<vector<double>>& distancias, int depotId) {
-    std::vector<Saving> savings;
+vector<vector<int>> ordenarPorDistancias(const vector<vector<double>>& distancias){
     int n = distancias.size();
+    vector<vector<int>> nodos_ordenados(n, vector<int>(n, 0)); // inicializa todo en 0
 
-    for (int i = 0; i < n; ++i) {
-        if (i != depotId) {
-            for (int j = i + 1; j < n; ++j) {
-                if (j != depotId) {
-                    double sij = distancias[depotId][i] + distancias[depotId][j] - distancias[i][j]; // calcula el ahorro
-                    savings.push_back({i, j, sij});
-                }
+    for (int i = 1; i < n; i++) {
+        vector<std::pair<double, int>> distanciasDesdeI;
+        for (int j = 1; j < n; j++) {
+            if (i != j) {
+                distanciasDesdeI.push_back({distancias[i][j], j});
             }
         }
+        
+        // ordenar la fila por distancia creciente
+        sort(distanciasDesdeI.begin(), distanciasDesdeI.end()); // primero ordena por distancia. Si empata, ordena por numero de cliente
+
+        int id = 1;
+        // guardar solo los índices de los clientes
+        for (auto it = distanciasDesdeI.begin(); it != distanciasDesdeI.end(); ++it) {
+            nodos_ordenados[i][id] = it->second;
+            id++;
+        }
     }
-
-    sort(savings.begin(), savings.end()); // ordena de mayor a menor
-
-    return savings;
+    return nodos_ordenados;
 }
 
 vector<Route> vecinoMasCercano(const VRPLIBReader& instancia) {
@@ -41,39 +44,36 @@ vector<Route> vecinoMasCercano(const VRPLIBReader& instancia) {
         if (i != depot) {
             rutaCliente[i] = Route{{depot, i, depot}, demandas[i]}; // seteamos las rutas iniciales
         }
+    } // ESTA PARTE NO VA
+
+    // construimos una matriz de clientes ordenados por distancia, para cada cliente 
+    vector<vector<int>> clientesOrdenadosPorDistancia = ordenarPorDistancias(distancias);
+    vector<int> clientesNoVisitados = {};
+    for (int i = 1; i <= n; i++) {
+        if (i != depot) {
+            clientesNoVisitados.push_back(i); 
+            // inicializo los clientes no visitados como todos los clientes menos el depot
+        }
+    } 
+
+    while (clientesNoVisitados.size() > 0) {
+        Route rutaActual;
+        rutaActual.nodes.push_back(depot);
+        rutaActual.totalDemand = 0;
+        int actual = depot;
+        int capacidadRestante = capacidad;
+
+        while ()
     }
-
-    vector<vector<double>> distancias_ordenadas = {};
-    vector<vector<double>> distancias_copia = {};
-
-    vector<vector<int>> calcularVecinosMasCercanos(const vector<vector<double>>& distancias){
-        int n = distancias.size();
-        vector<vector<int>> vecinosMasCercanos(n);
-
-        for (int i = 0; i < n; ++i) {
-            vector<std::pair<double, int>> distanciasDesdeI;
-            for (int j = 0; j < n; ++j) {
-                if (i != j) {
-                    distanciasDesdeI.push_back({distancias[i][j], j});
-                }
-            }
-
-            // ordenar la fila por distancia creciente
-            sort(distanciasDesdeI.begin(), distanciasDesdeI.end());
-
-            // guardar solo los índices de los vecinos
-            for (const auto& par : distanciasDesdeI) {
-                vecinosMasCercanos[i].push_back(par.second);
-            }   
-    }
-
-    return vecinosMasCercanos;
+    
 }
     
     // empiezo del depot, veo el nodo con min distancia y agregarlo a la ruta
     
     // ahora me paro en el ultimo que agregue. busco el nodo con min distancia a este nodo, 
     // si veo que al agregarlo no me paso de la capacidad, lo agrego. sino paso al prox min
+
+    nodo_ordenados [i][0]
     vector<int> clientes_visitados;
     for (int r = 0; r <= n; r++) {
         
